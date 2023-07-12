e5d10d7643f03698d9310c56235f5ad5c93b6086
RailgunSinon <sinonsniper@yandex.ru>
2023-07-12 14:15:31 +0300

    BugFix: Успех, чистка мусора 2

--- stat-service/stat-server/src/main/java/ru/practicum/explorewithme/statserver/storage/HitRepository.java
+++ stat-service/stat-server/src/main/java/ru/practicum/explorewithme/statserver/storage/HitRepository.java
@@ -10,14 +10,15 @@ import ru.practicum.explorewithme.statserver.model.Hit;
 
 @Repository
 public interface HitRepository extends JpaRepository<Hit, Long> {
-    
+
     @Query("SELECT new ru.practicum.explorewithme.dto.StateViewDto(h.app,h.uri,COUNT(h.ip)) "
         + "FROM Hit h WHERE h.created BETWEEN ?1 AND ?2 AND (h.uri IN ?3 OR ?3 IS NULL) "
         + "GROUP BY h.app,h.uri ORDER BY COUNT(h.ip) DESC")
     List<StateViewDto> findHitsNoUniq(LocalDateTime start, LocalDateTime end, List<String> uris);
 
-    @Query("SELECT new ru.practicum.explorewithme.dto.StateViewDto(h.app,h.uri,COUNT(DISTINCT(h.ip))) "
-        + "FROM Hit h WHERE h.created BETWEEN ?1 AND ?2 AND (h.uri IN ?3 OR ?3 IS NULL) "
-        + "GROUP BY h.app,h.uri ORDER BY COUNT(DISTINCT(h.ip)) DESC ")
+    @Query(
+        "SELECT new ru.practicum.explorewithme.dto.StateViewDto(h.app,h.uri,COUNT(DISTINCT(h.ip))) "
+            + "FROM Hit h WHERE h.created BETWEEN ?1 AND ?2 AND (h.uri IN ?3 OR ?3 IS NULL) "
+            + "GROUP BY h.app,h.uri ORDER BY COUNT(DISTINCT(h.ip)) DESC ")
     List<StateViewDto> findHitsUniq(LocalDateTime start, LocalDateTime end, List<String> uris);
 }
